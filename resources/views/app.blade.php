cf7c218b188e24be8e524a86cda7d9e1910033e3
tonggan198699 <gantong1986@gmail.com>
2018-01-17 18:27:06 +0000

    Create app as a master layout to extend the contact form

---
+++ resources/views/app.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>App Layout Page</title>
+
+    <!-- google font -->
+    <link href="https://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic" rel="stylesheet" type="text/css">
+    <link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.js"></script>
+    <link href="/css/app.css" rel="stylesheet">
+
+
+</head>
+<body>
+        @yield('content')
+
+    <script src="/js/app.js"></script>
+
+</body>
+</html>
